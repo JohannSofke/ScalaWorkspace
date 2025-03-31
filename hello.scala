1ddceae7296ae0c6f1779c792ebd68573cc6d8b2
JohannSofke <79168826+JohannSofke@users.noreply.github.com>
2025-03-31 16:28:47 +0000

    Autoformatierung

--- hello.scala
+++ hello.scala
@@ -1,18 +1,15 @@
 @main
-def hello():Unit=
-    init()
-    while (true)
-        loop()
-        Thread.sleep(500)
- 
-def init():Unit =
-    println("Einmal")
+def hello(): Unit =
+  init()
+  while (true)
+    loop()
+    Thread.sleep(500)
+
+def init(): Unit =
+  println("Einmal")
 
 import scala.util.Random
 
-def loop():Unit=
-    println("\u001bc")
-    println(Random.nextInt())
-        
-
-
+def loop(): Unit =
+  println("\u001bc")
+  println(Random.nextInt())
