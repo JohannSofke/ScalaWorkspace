22d699649839d288d9fbf54b5834e76e972a06c8
JohannSofke <79168826+JohannSofke@users.noreply.github.com>
2025-03-31 16:26:27 +0000

    Eventloop mit Initialisirung

---
+++ hello.scala
@@ -0,0 +1,18 @@
+@main
+def hello():Unit=
+    init()
+    while (true)
+        loop()
+        Thread.sleep(500)
+ 
+def init():Unit =
+    println("Einmal")
+
+import scala.util.Random
+
+def loop():Unit=
+    println("\u001bc")
+    println(Random.nextInt())
+        
+
+
