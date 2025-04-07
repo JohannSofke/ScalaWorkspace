0b8d4a073531a954695505be63763afeed1d14a7
JohannSofke <79168826+JohannSofke@users.noreply.github.com>
2025-04-07 17:46:14 +0000

    Quadriere Zufallszahl zwischen 0 und 10

--- gol.scala
+++ gol.scala
@@ -1,16 +1,20 @@
 package gol
 
 import scala.util.Random
+import scala.compiletime.ops.int
 
 def init(): Unit =
   println("Einmal")
 
 def loop(): Unit =
   clearScreen()
-  println(Random.nextInt())
+  println(square(Random.nextInt(11)))
 
 def delay(): Unit =
   Thread.sleep(500)
 
 private def clearScreen(): Unit =
   println("\u001bc")
+
+private def square(x: Int): Int =
+  x * x
