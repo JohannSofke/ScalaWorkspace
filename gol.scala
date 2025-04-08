27f4a7aa00993fadb0261679b6786c303ea0489b
JohannSofke <79168826+JohannSofke@users.noreply.github.com>
2025-04-08 15:34:23 +0000

    Drucke Characters, die jeweils zwei Zellen repräsentieren

--- gol.scala
+++ gol.scala
@@ -3,18 +3,32 @@ package gol
 import scala.util.Random
 import scala.compiletime.ops.int
 
+enum Character {
+  case On, Off, Up, Down
+}
+
 def init(): Unit =
   println("Einmal")
 
 def loop(): Unit =
   clearScreen()
-  println(square(Random.nextInt(11)))
+  printCharacter(Character.On)
+  printCharacter(Character.Off)
+  printCharacter(Character.Up)
+  printCharacter(Character.Down)
 
 def delay(): Unit =
   Thread.sleep(500)
 
 private def clearScreen(): Unit =
   println("\u001bc")
+
+def printCharacter(c: Character): Unit =
+  c match
+    case Character.On  => print("█")
+    case Character.Off => print(" ")
+    case Character.Up  => print("▀")
+    case Character.Down => print("▄")
 
 private def square(x: Int): Int =
   x * x
